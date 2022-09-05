706046cc2d8e3e3b1a92ccc9c3c58a88b1f53fb0
Deepak Kataria <deepak.kataria@olx.com>
2022-09-06 01:29:47 +0530

    Changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1 style='text-align: center;'>Hello World, my name is Deepak!</h1>"
+    return "<h1>Hello World, my name is Deepak!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) 
